5300d165cc1af72c008d9e2db2c3f52dbf824e82
Carlos Sierra <csierra@apache.org>
2019-02-14 16:04:53 +0100

    [ARIES-1892] Avoid NPE

--- jax-rs.whiteboard/src/main/java/org/apache/aries/jax/rs/whiteboard/internal/cxf/CxfJaxrsServiceRegistrator.java
+++ jax-rs.whiteboard/src/main/java/org/apache/aries/jax/rs/whiteboard/internal/cxf/CxfJaxrsServiceRegistrator.java
@@ -158,6 +158,10 @@ public class CxfJaxrsServiceRegistrator {
         ArrayList<Class<?>> classes = new ArrayList<>();
 
         for (ClassResourceInfo resourceInfo : classResourceInfo) {
+            if (resourceInfo == null) {
+                continue;
+            }
+
             ResourceProvider resourceProvider =
                 resourceInfo.getResourceProvider();
 
